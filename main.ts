f2aef91985a7eb997ff719e1b1428f63b838777b
Srikanth-Jatoth <srikanthjatoth09@gmail.com>
2024-11-04 22:59:30 -0500

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,15 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideAnimations(),
+    importProvidersFrom(MatSnackBarModule)
+  ]
+}).catch(err => console.error(err));
